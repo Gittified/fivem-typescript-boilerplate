3637ecfb33de9f2af8ef9254ba16f9924abec123
Nullified <28195796+Gittified@users.noreply.github.com>
2024-01-18 22:16:45 +0100

    fix(build): add context as of esbuild 0.17 update
    Signed-off-by: Nullified <28195796+Gittified@users.noreply.github.com>

--- build/build-bundle.js
+++ build/build-bundle.js
@@ -3,59 +3,49 @@ const esbuild = require("esbuild");
 const IS_WATCH_MODE = process.env.IS_WATCH_MODE;
 
 const TARGET_ENTRIES = [
-  {
-    target: "node16",
-    entryPoints: ["server/server.ts"],
-    platform: "node",
-    outfile: "./dist/server/server.js",
-  },
-  {
-    target: "es2020",
-    entryPoints: ["client/client.ts"],
-    outfile: "./dist/client/client.js",
-  },
+	{
+		target: "node16",
+		entryPoints: ["server/server.ts"],
+		platform: "node",
+		outfile: "./dist/server/server.js",
+	},
+	{
+		target: "es2020",
+		entryPoints: ["client/client.ts"],
+		outfile: "./dist/client/client.js",
+	},
 ];
 
 const buildBundle = async () => {
-  try {
-    const baseOptions = {
-      logLevel: "info",
-      bundle: true,
-      charset: "utf8",
-      minifyWhitespace: true,
-      absWorkingDir: process.cwd(),
-    };
+	try {
+		const baseOptions = {
+			logLevel: "info",
+			bundle: true,
+			charset: "utf8",
+			minifyWhitespace: true,
+			absWorkingDir: process.cwd(),
+		};
 
-    for (const targetOpts of TARGET_ENTRIES) {
-      const mergedOpts = { ...baseOptions, ...targetOpts };
+		for (const targetOpts of TARGET_ENTRIES) {
+			const ctx = await esbuild.context({ ...baseOptions, ...targetOpts });
 
-      if (IS_WATCH_MODE) {
-        mergedOpts.watch = {
-          onRebuild(error) {
-            if (error)
-              console.error(
-                `[ESBuild Watch] (${targetOpts.entryPoints[0]}) Failed to rebuild bundle`
-              );
-            else
-              console.log(
-                `[ESBuild Watch] (${targetOpts.entryPoints[0]}) Sucessfully rebuilt bundle`
-              );
-          },
-        };
-      }
+			if (IS_WATCH_MODE) {
+				await ctx.watch();
+			} else {
+				const { errors } = await ctx.rebuild();
 
-      const { errors } = await esbuild.build(mergedOpts);
+				if (errors.length >= 1) {
+					console.error(`[ESBuild] Bundle failed with ${errors.length} errors`);
+				}
 
-      if (errors.length) {
-        console.error(`[ESBuild] Bundle failed with ${errors.length} errors`);
-        process.exit(1);
-      }
-    }
-  } catch (e) {
-    console.log("[ESBuild] Build failed with error");
-    console.error(e);
-    process.exit(1);
-  }
+				ctx.dispose();
+			}
+		}
+	} catch (e) {
+		console.log("[ESBuild] Build failed with error");
+		console.error(e);
+		process.exit(1);
+	}
 };
 
 buildBundle().catch(() => process.exit(1));
